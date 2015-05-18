8331554544bbfecfdef96e7ade43f785170081a5
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-05-19 00:17:11 +0200

    home, leitbild

--- src/pages/public.js
+++ src/pages/public.js
@@ -8,7 +8,49 @@ export default React.createClass({
 
     render () {
         return (
-            <h1>Public</h1>
+            <div id='home' className='page home'>
+                <div className='jumbotron'>
+                    <h1>Gabriel Software</h1>
+                    <ul>
+                        <li>einfach</li>
+                        <li>individuell</li>
+                        <li>zuverlässig</li>
+                    </ul>
+                    <p className='lead'>...damit Sie sich auf Ihre Arbeit konzen&shy;trieren können</p>
+                </div>
+
+                <div className='row marketing'>
+                    <div className='col-lg-4'>
+                        <div className='glyphicon glyphicon-cloud' style={{fontSize: 88 + 'px'}}></div>
+                        <h4 style={{marginTop: 0 + 'px'}}>Web-Anwendungen</h4>
+                        <ul>
+                            <li>zweckmässige Datenbank&shy;technologie</li>
+                            <li>lokal installiert, im Intranet oder im Internet</li>
+                            <li>professionell aufgebaut und dokumentiert</li>
+                        </ul>
+                    </div>
+                    <div className='col-lg-4'>
+                        <img src='/images/access.png' alt='' height='82' width='82'/>
+                        <h4>Access-Anwendungen</h4>
+                        <ul>
+                            <li>firmenintern</li>
+                            <li>rasche Entwicklung</li>
+                            <li>Prototypen für Web-Anwendungen</li>
+                        </ul>
+                    </div>
+                    <div className='col-lg-4'>
+                        <div className='glyphicon glyphicon-comment' style={{fontSize: 78 + 'px'}}></div>
+                        <h4 style={{marginBottom: 0 + 'px'}}>Beratung</h4>
+                        <p>bei der Entwicklung von Software:</p>
+                        <ul>
+                            <li>Bedürfnisse abklären...</li>
+                            <li>...und in den 'Fachjargon' von Software&shy;ingenieuren übersetzen</li>
+                            <li>Risiken minimieren</li>
+                            <li>unabhängige Meinung</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         )
     }
 })
