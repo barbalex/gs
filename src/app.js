57dce9632760011be1083a198c6887d33f8a3f54
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-05-21 17:44:29 +0200

    Menu Mobilansicht managen

--- src/app.js
+++ src/app.js
@@ -2,16 +2,39 @@
 'use strict'
 
 import app from 'ampersand-app'
+import $ from 'jquery'
 // this import seems not used but is necessary for styling
 import styles from './styles/main.styl'
 import Router from './router'
 
 window.app = app // expose app to the browser console
 
+function closeNavbarCollapse ($navbarCollapse) {
+  // nur schliessen, wenn Mobilansicht aktiv
+  if (window.innerWidth < 767) {
+    $navbarCollapse.hide(400)
+  }
+}
+
 app.extend({
   init () {
     this.router = new Router()
     this.router.history.start()
+    // bootstrap managt die navbar nicht
+    // selber machen!
+    $(document).on('click.nav', '.navbar-nav', function () {
+      const $navbarCollapse = $(this).closest('.navbar-collapse')
+      closeNavbarCollapse($navbarCollapse)
+    })
+    $(document).on('click.nav', '.navbar-toggle', function () {
+      const $navbarCollapse = $(this).parent().parent().find('.navbar-collapse')
+      $navbarCollapse.show(400)
+    })
+    // man soll auch auf den Titel klicken können und das Menü schliesst
+    $(document).on('click.nav', '.navbar-brand', function () {
+      const $navbarCollapse = $(this).parent().parent().find('.navbar-collapse')
+      closeNavbarCollapse($navbarCollapse)
+    })
   }
 })
 
