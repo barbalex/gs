5e55048a8f9c1a29b886d2c3112f4311da105672
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-06-03 13:47:37 +0200

    verify domain ownership to google

---
+++ gulp-tasks/copy_google_verif.js
@@ -0,0 +1,14 @@
+/*
+ * alex@gabriel-software.ch is managed by google
+ * to verify me as account owner google62104c6b2995c087.html
+ * needs to be accessible in the domain root
+ */
+
+'use strict'
+
+var gulp = require('gulp')
+
+gulp.task('copy_google_verif', function () {
+  return gulp.src('./google62104c6b2995c087.html')
+    .pipe(gulp.dest('public/'))
+})
