4b92108e9caebd8a9cc3c0c682b9f2a4a028bb68
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-12 21:25:17 +0100

    remove sensor option

--- src/modules/prepareGoogleMap.js
+++ src/modules/prepareGoogleMap.js
@@ -11,7 +11,7 @@ export default function () {
     const script = document.createElement('script')
     script.type = 'text/javascript'
     // load google maps api script then show the map
-    script.src = 'https://maps.googleapis.com/maps/api/js?sensor=false&callback=showGoogleMap'
+    script.src = 'https://maps.googleapis.com/maps/api/js?callback=showGoogleMap'
     document.body.appendChild(script)
   }
 }
