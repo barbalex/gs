fd3e29a034fa8e45376d87dc7dd5b42e0d818dbb
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-22 23:24:27 +0200

    modularize home

---
+++ src/pages/home.js
@@ -0,0 +1,107 @@
+'use strict'
+
+import React from 'react'
+import Navbar from 'react-bootstrap/lib/Navbar'
+import { Link } from 'react-router'
+
+const Home = React.createClass({
+  displayName: 'Home',
+
+  render() {
+    return (
+      <div>
+        <div
+          className='header'
+        >
+          <Navbar
+            inverse
+            fixedTop
+          >
+            <div
+              className='container'
+            >
+              <div
+                className='navbar-header'
+              >
+                <button
+                  type='button'
+                  className='navbar-toggle'
+                  data-toggle='collapse'
+                  data-target='.navbar-collapse'
+                >
+                  <span
+                    className='sr-only'
+                  >
+                    Toggle navigation
+                  </span>
+                  <span className='icon-bar'/>
+                  <span className='icon-bar'/>
+                  <span className='icon-bar'/>
+                </button>
+                <Link
+                  to='/'
+                  className='navbar-brand'
+                >
+                  Gabriel Software
+                </Link>
+              </div>
+              <div
+                className='collapse navbar-collapse'
+              >
+                <ul
+                  className='nav navbar-nav'
+                >
+                  <li>
+                    <Link
+                      to='leitbild'
+                      activeClassName='active'
+                    >
+                      Leitbild
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to='projekte'
+                      activeClassName='active'
+                    >
+                      Projekte
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to='technologien'
+                      activeClassName='active'
+                    >
+                      Technologien
+                    </Link>
+                  </li>
+                </ul>
+                <ul
+                  className='nav navbar-nav navbar-right'
+                >
+                  <li
+                    className='kontaktNav'
+                  >
+                    <Link
+                      to='kontakt'
+                      activeClassName='active'
+                    >
+                      Kontakt
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Navbar>
+        </div>
+        <div
+          className='container'
+        >
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+})
+
+export default Home
--- src/router.js
+++ src/router.js
@@ -2,8 +2,8 @@
 
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router'
-import Navbar from 'react-bootstrap/lib/Navbar'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import Home from './pages/home'
 import PublicPage from './pages/public'
 import LeitbildPage from './pages/leitbild'
 import ProjektePage from './pages/projekte'
@@ -12,113 +12,13 @@ import KontaktPage from './pages/kontakt'
 import FourOhFourPage from './pages/fourOhFour'
 
 export default function () {
-  const App = React.createClass({
-    displayName: 'HomePage',
-
-    render () {
-      return (
-        <div>
-          <div
-            className='header'
-          >
-            <Navbar
-              inverse
-              fixedTop
-            >
-              <div
-                className='container'
-              >
-                <div
-                  className='navbar-header'
-                >
-                  <button
-                    type='button'
-                    className='navbar-toggle'
-                    data-toggle='collapse'
-                    data-target='.navbar-collapse'
-                  >
-                    <span
-                      className='sr-only'
-                    >
-                      Toggle navigation
-                    </span>
-                    <span className='icon-bar'/>
-                    <span className='icon-bar'/>
-                    <span className='icon-bar'/>
-                  </button>
-                  <Link
-                    to='/'
-                    className='navbar-brand'
-                  >
-                    Gabriel Software
-                  </Link>
-                </div>
-                <div
-                  className='collapse navbar-collapse'
-                >
-                  <ul
-                    className='nav navbar-nav'
-                  >
-                    <li>
-                      <Link
-                        to='leitbild'
-                        activeClassName='active'
-                      >
-                        Leitbild
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to='projekte'
-                        activeClassName='active'
-                      >
-                        Projekte
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to='technologien'
-                        activeClassName='active'
-                      >
-                        Technologien
-                      </Link>
-                    </li>
-                  </ul>
-                  <ul
-                    className='nav navbar-nav navbar-right'
-                  >
-                    <li
-                      className='kontaktNav'
-                    >
-                      <Link
-                        to='kontakt'
-                        activeClassName='active'
-                      >
-                        Kontakt
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </Navbar>
-          </div>
-          <div
-            className='container'
-          >
-            {this.props.children}
-          </div>
-        </div>
-      )
-    }
-  })
-
   render(
     <Router
       history={browserHistory}
     >
       <Route
         path='/'
-        component={App}
+        component={Home}
       >
         <IndexRoute
           component={PublicPage}/
