4cd926d65a680df0b49c18b2ddb41e61d35c0855
Alexander Gabriel <alex@gabriel-software.ch>
2016-07-05 10:49:32 +0200

    toggle navbarCollapse

--- src/modules/manageNavbarToggleVisibility.js
+++ src/modules/manageNavbarToggleVisibility.js
@@ -7,7 +7,7 @@
 
 import $ from 'jquery'
 
-function closeNavbarCollapse ($navbarCollapse) {
+const closeNavbarCollapse = ($navbarCollapse) => {
   // nur schliessen, wenn Mobilansicht aktiv
   if (window.innerWidth < 767) {
     $navbarCollapse.hide(400)
@@ -17,17 +17,28 @@ function closeNavbarCollapse ($navbarCollapse) {
 export default function () {
   // bootstrap managt die navbar nicht
   // selber machen!
-  $(document).on('click.nav', '.navbar-nav', function () {
+  $(document).on('click.nav', '.navbar-nav', function() {
     const $navbarCollapse = $(this).closest('.navbar-collapse')
     closeNavbarCollapse($navbarCollapse)
   })
-  $(document).on('click.nav', '.navbar-toggle', function () {
-    const $navbarCollapse = $(this).parent().parent().find('.navbar-collapse')
-    $navbarCollapse.show(400)
+  $(document).on('click.nav', '.navbar-toggle', function() {
+    const $navbarCollapse = $(this)
+      .parent()
+      .parent()
+      .find('.navbar-collapse')
+
+    if ($navbarCollapse.is(':visible')) {
+      $navbarCollapse.hide(400)
+    } else {
+      $navbarCollapse.show(400)
+    }
   })
   // man soll auch auf den Titel klicken können und das Menü schliesst
-  $(document).on('click.nav', '.navbar-brand', function () {
-    const $navbarCollapse = $(this).parent().parent().find('.navbar-collapse')
+  $(document).on('click.nav', '.navbar-brand', function() {
+    const $navbarCollapse = $(this)
+      .parent()
+      .parent()
+      .find('.navbar-collapse')
     closeNavbarCollapse($navbarCollapse)
   })
 }
